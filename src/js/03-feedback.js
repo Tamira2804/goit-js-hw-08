7659c986bc225251132e1bd38a1b185e3c03210c
Tamira2804 <tamira2804@gmail.com>
2023-06-05 23:11:34 +0200

    feedback-form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const emailInputEl = document.querySelector('input[type="email"]');
+const messageInputEl = document.querySelector('textarea[name="message"]');
+const formEl = document.querySelector('.feedback-form');
+
+window.addEventListener('load', () => {
+  const storedFormData = localStorage.getItem('feedback-form-state');
+
+  if (storedFormData) {
+    const formData = JSON.parse(storedFormData);
+    emailInputEl.value = formData.email;
+    messageInputEl.value = formData.message;
+  }
+});
+
+formEl.addEventListener('input', throttle(handleFormInput, 500));
+
+function handleFormInput() {
+  const emailValue = emailInputEl.value;
+  const messageValue = messageInputEl.value;
+
+  const formData = {
+    email: emailValue,
+    message: messageValue,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const emailValue = emailInputEl.value;
+  const messageValue = messageInputEl.value;
+
+  const formData = {
+    email: emailValue,
+    message: messageValue,
+  };
+
+  console.log(formData);
+
+  emailInputEl.value = '';
+  messageInputEl.value = '';
+
+  localStorage.removeItem('feedback-form-state');
+}
